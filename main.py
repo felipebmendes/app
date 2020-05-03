2a5b939209b299844d8b8c5dda5ad38a5d7414b4
felipebmendes <fmendes@totvs.com.br>
2020-05-03 17:31:12 -0300

    print

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from app import create_app
 
 application = create_app()
 
-print('starting')
+print('starting 1, 2, 3.')
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', port=5000)
